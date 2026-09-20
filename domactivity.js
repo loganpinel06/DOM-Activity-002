fe8e2526301e6465ed8da8da62fd09862ba2ab15
Logan Pinel <loganpinel@outlook.com>
2026-09-20 14:52:02 -0400

    FFIX - Reverted the changes to the json file to support using json.parse()

--- domactivity.js
+++ domactivity.js
@@ -1,1 +1,11 @@
 /* add your code here */
+//Logan Pinel
+//DOM-Activity-002
+//09-17-26
+
+//DOM Content Loader
+document.addEventListener("DOMContentLoaded", function () {
+    //parse the json file for the paintings
+    const paintings = JSON.parse(content);
+
+});
